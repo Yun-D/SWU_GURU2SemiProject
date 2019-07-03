513bbf44035b0cfabccfa0f59ea6709333f653ca
HeoYunJeong <yund_272@naver.com>
2019-07-03 16:54:26 +0900

    1. xml, java 파일 생성 (TabView 및 ListView 부분 구현해야 함) 2. drawable 임시 이미지 추가

---
+++ app/src/main/java/com/example/semiproject2019/MemberInfo.java
@@ -0,0 +1,15 @@
+package com.example.semiproject2019;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MemberInfo extends AppCompatActivity {
+    //회원 정보
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_info);
+    }
+}

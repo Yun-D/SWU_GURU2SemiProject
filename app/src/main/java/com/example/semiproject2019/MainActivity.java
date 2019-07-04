703188b76afcf19df66abfd92502009fe7b0c339
HeoYunJeong <yund_272@naver.com>
2019-07-04 16:51:05 +0900

    1. 로그인 수정중 2. setMember 대신 List 사용하기?

--- app/src/main/java/com/example/semiproject2019/MainActivity.java
+++ app/src/main/java/com/example/semiproject2019/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.semiproject2019.database.Database;
+import com.example.semiproject2019.model.MemberModel;
+
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName(); //클래스명 획득
+
+    EditText login_ID, login_PWD;
+    Button btnLogin, btnJoin_Activity;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,15 +42,33 @@ public class MainActivity extends AppCompatActivity {
 전체적으로 - 메모탭 / 회원정보 탭 임
 
          */
-        Button btnLogin = findViewById(R.id.btnLogin); //로그인 버튼
+        login_ID = findViewById(R.id.login_ID);
+        login_PWD = findViewById(R.id.login_PWD);
+
+        btnLogin = findViewById(R.id.btnLogin); //로그인 버튼
+
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
+            Database db = Database.getInstance(getApplicationContext());
+
             @Override
             public void onClick(View view) {
-                Toast.makeText(getApplicationContext(), "로그인...", Toast.LENGTH_SHORT).show();
+                MemberModel savedMember = db.getMember(login_ID.getText().toString());
+                if (savedMember != null) {
+                    Log.d(TAG, "savedMember = " + savedMember.toString());
+                }
+                else {
+                    Log.d(TAG, "회원정보 없음.");
+                }
+//
+//              Intent intent = new Intent(getApplicationContext(), TabActivity.class);
+//              startActivity(intent);
+//                }
+
             }
         });
 
-        Button btnJoin_Activity = findViewById(R.id.btnJoin_Activity); //회원가입 버튼
+        btnJoin_Activity = findViewById(R.id.btnJoin_Activity); //회원가입 버튼
         btnJoin_Activity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {

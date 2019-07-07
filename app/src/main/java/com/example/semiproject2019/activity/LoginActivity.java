2292922b37565282de6da2b92e86d0ea091fa130
HeoYunJeong <yund_272@naver.com>
2019-07-08 01:22:23 +0900

    로그인 코드 수정

--- app/src/main/java/com/example/semiproject2019/activity/LoginActivity.java
+++ app/src/main/java/com/example/semiproject2019/activity/LoginActivity.java
@@ -4,21 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.semiproject2019.R;
+import com.example.semiproject2019.bean.MemberBean;
 import com.example.semiproject2019.database.Database;
+import com.example.semiproject2019.db.FileDB;
 import com.example.semiproject2019.model.MemberModel;
 
 public class LoginActivity extends AppCompatActivity {
     private final String TAG = this.getClass().getSimpleName(); //클래스명 획득
 
     EditText login_ID, login_PWD;
-    Button btnLogin, btnJoin_Activity;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,34 +30,43 @@ public class LoginActivity extends AppCompatActivity {
         login_ID = findViewById(R.id.login_ID);
         login_PWD = findViewById(R.id.login_PWD);
 
-        btnLogin = findViewById(R.id.btnLogin); //로그인 버튼
+        Button btnLogin = findViewById(R.id.btnLogin);
+        Button btnJoin_Activiy = findViewById(R.id.btnJoin_Activity);
 
-
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            Database db = Database.getInstance(getApplicationContext());
-
-            @Override
-            public void onClick(View view) {
-                MemberModel savedMember = db.getMember(login_ID.getText().toString());
-                if (savedMember != null) {
-                    Log.d(TAG, "savedMember = " + savedMember.toString());
-                }
-                else {
-                    Log.d(TAG, "회원정보 없음.");
-                }
-
-            }
-        });
-
-        btnJoin_Activity = findViewById(R.id.btnJoin_Activity); //회원가입 버튼
-        btnJoin_Activity.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), JoinActivity.class);
-                startActivity(intent);
-            }
-        });
-
-
+        btnLogin.setOnClickListener(mBtnLoginClick);
+        btnJoin_Activiy.setOnClickListener(mBtnJoinClick);
     }
+
+    private View.OnClickListener mBtnLoginClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            String memID = login_ID.getText().toString();
+            String memPW = login_PWD.getText().toString();
+
+            MemberBean memberBean = FileDB.getFindMember(LoginActivity.this, memID);
+            if(memberBean == null) {
+                Toast.makeText(LoginActivity.this, "해당 아이디는 가입이 되어있지 않습니다. ", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            //패스워드 비교
+            if (TextUtils.equals(memberBean.memPWD, memPW)) {
+                FileDB.setLoginMember(LoginActivity.this, memberBean); //저장
+
+                //비밀번호 일치
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(LoginActivity.this, "패스워드가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        }
+    };
+
+    private View.OnClickListener mBtnJoinClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(LoginActivity.this, CameraCapture2Activity.class);
+            startActivity(intent);
+        }
+    };
 }
